8bc891da64ff79bc917ab28d93d8902821ccee31
TTAJ1b14 <a.p.grishukevich@gmail.com>
2023-06-02 18:28:33 +0300

    Enemy types
    Added 3 Enemy types ang logic to summon them depending on wave difficulty

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrafab;
+    public GameObject[] enemyPrefab;
     public GameObject powerUpPrefab;
 
     private float spawnRange = 9;
-    
+
     public int enemyCount;
     public int waveNumber;
+
+    private int difficult;
 
 
     // Start is called before the first frame update
@@ -19,26 +21,44 @@ public class SpawnManager : MonoBehaviour
         SpawnEnemyWave(waveNumber);
         Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
     }
+
+    //limit current difficulty to be no longer than array length
+    private int DifficultLevel()
+    {
+        if (difficult > enemyPrefab.Length)
+        {
+            difficult = enemyPrefab.Length;
+        }
+        return difficult;
+    }
+
+    //widen list of available enemies
+    private int GenerateRandomEnemy()
+    {
+        int randomEnemy = Random.Range(0, DifficultLevel());
+        return randomEnemy;
+    }
+
+    //using it to spawn an enemy
     void SpawnEnemyWave(int enemiesToSpawn)
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(enemyPrafab, GenerateSpawnPosition(), enemyPrafab.transform.rotation);
+            Instantiate(enemyPrefab[GenerateRandomEnemy()], GenerateSpawnPosition(), enemyPrefab[GenerateRandomEnemy()].transform.rotation);
         }
     }
 
-    // Update is called once per frame
+    //increase difficulty depending on the waveNumber
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
         if (enemyCount == 0)
         {
             waveNumber++;
+            difficult = waveNumber / 2;
             SpawnEnemyWave(waveNumber);
             Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
-
         }
-
     }
 
     private Vector3 GenerateSpawnPosition()
